5884e4a7cd66b2c61e959559e9b65c191a40b2b4
Mwaleed-001 <waleedabdullahbrand@gmail.com>
2026-05-13 18:32:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let a = document.getElementById("hd")
+let x = document.getElementById("para")
+
+
+
+            // QUERY SELECTORS
+// let a = document.querySelectorAll(".pp")[0]
+// let a = document.querySelectorAll("#hd")[0]
+
+
+
+            // CSS STRYLINGS
+// a.style.color="Blue"
+// x.style.backgroundColor="gray"
+
+
+
+        // CLASS NAME & CLASS LIST
+// a.className="abc"
+// a.classList.add("abc","xyz")
+// a.classList.remove("abc")
+
+
+
+function clk(){
+// a.classList.add("abc","xyz")
+// a.classList.toggle("abc")
+}
+
+
+
+
+
+
+
+
+
+// let y = document.getElementById("anc")
+// y.setAttribute("href","https://pccmbd.com")
+// console.log(y.getAttribute("href"))
