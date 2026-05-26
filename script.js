0c0f8468f2f2c221a85b772d617921bff4ee2a80
Mwaleed-001 <waleedabdullahbrand@gmail.com>
2026-05-26 14:03:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-                       // ADD EVENT LISTENER
-let a = document.getElementById("btn")
-a.addEventListener("click", function(){
-    console.log("Clicked")
-})
-a.addEventListener("click", function(){
-    alert("Mouse a gyaaa!")
-})
+            // ARRAYS
+
+            // example1===
+// let a = [10, 20, 30];
+// console.log(a[1])
+// let std = ["Ali", "Maths", 75]
+// console.log(std[0])
+
+
+         //  example2===
+// let names = [];
+// names[0] = "Waleed";
+// names[1] = "Ali";
+// console.log(names);
+
+
+        // example3==
+// let names = ["waleed", "ali", "saad","hassan"];
+// console.log(names);
+// console.log(names[3]);
+    
+// let sth = [3];
+// console.log(sth);
+
+// console.log(names.length);
+
+
+
+
+            // OBJECTS
+// let std = {
+//     Name :  "Ali",
+//     Subject : "English",
+//     Marks   :   88,
+//     Hobbies :   ["Reading", "Writting", "Coding"]
+// }
+
+// console.log(std.Hobbies[2])
+
+
+
+
