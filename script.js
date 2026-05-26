87438fb5db70dd9e34ccdd0cc3a3dee65c17770b
Mwaleed-001 <waleedabdullahbrand@gmail.com>
2026-05-26 14:02:28 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,8 @@
-let a = document.getElementById("hd")
-let x = document.getElementById("para")
-
-
-
-            // QUERY SELECTORS
-// let a = document.querySelectorAll(".pp")[0]
-// let a = document.querySelectorAll("#hd")[0]
-
-
-
-            // CSS STRYLINGS
-// a.style.color="Blue"
-// x.style.backgroundColor="gray"
-
-
-
-        // CLASS NAME & CLASS LIST
-// a.className="abc"
-// a.classList.add("abc","xyz")
-// a.classList.remove("abc")
-
-
-
-function clk(){
-// a.classList.add("abc","xyz")
-// a.classList.toggle("abc")
-}
-
-
-
-
-
-
-
-
-
-// let y = document.getElementById("anc")
-// y.setAttribute("href","https://pccmbd.com")
-// console.log(y.getAttribute("href"))
+                       // ADD EVENT LISTENER
+let a = document.getElementById("btn")
+a.addEventListener("click", function(){
+    console.log("Clicked")
+})
+a.addEventListener("click", function(){
+    alert("Mouse a gyaaa!")
+})
